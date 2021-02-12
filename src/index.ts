2433a50ff85b7e2a7e621c108114b924cb81c8e4
Michael LeCompte <michaelslec98@gmail.com>
2021-02-11 23:41:51 -0800

    feat: moved mikro-orm configuration to separate file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
 
 async function main() {
-  const orm = await MikroORM.init({
-    entities: [Post],
-    dbName: "reddit-clone",
-    type: "postgresql",
-    debug: !__prod__,
-  });
+  const orm = await MikroORM.init(microConfig);
+
+  const post = orm.em.create(Post, { title: "my first post" });
+  await orm.em.persistAndFlush(post);
 }
 
 main();
