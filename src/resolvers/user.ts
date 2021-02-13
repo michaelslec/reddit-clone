fcba894680e281dec930fd30d82b694729eb9df9
Michael LeCompte <michaelslec98@gmail.com>
2021-02-13 05:53:25 -0800

    fix: setting password to promise
    Didn't await argon2.hash(), so passwords were [object Promise]

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -17,8 +17,8 @@ export class UserResolver {
   async register(
     @Arg("options") options: UsernamePasswordInput,
     @Ctx() { em }: MyCtx
-  ) {
-    const hashedPassword = argon2.hash(options.password);
+  ): Promise<User> {
+    const hashedPassword = await argon2.hash(options.password);
     const user = em.create(User, {
       username: options.username,
       password: hashedPassword,
