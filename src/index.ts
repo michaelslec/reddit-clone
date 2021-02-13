3e8fd3235f8c9e3a9d1b3e5de12e5c024e8207c4
Michael LeCompte <michaelslec98@gmail.com>
2021-02-13 03:26:02 -0800

    feat: getting started with express

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 
 async function main() {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, { title: "my first post" });
-  await orm.em.persistAndFlush(post);
 }
 
 main();
