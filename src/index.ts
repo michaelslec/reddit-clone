39aca0b74134b41992fc47f4daeb4365ae0450b9
Michael LeCompte <michaelslec98@gmail.com>
2021-02-13 03:39:12 -0800

    feat: added initial graphql hookups

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 async function main() {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
+
+  const app = express();
+
+  const server = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  server.applyMiddleware({ app });
+
+  app.listen(8080, () => {
+    console.log("server started on http://localhost:8080");
+  });
 }
 
 main();
