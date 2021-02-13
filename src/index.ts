7be5bdfc88327961174c00a5b94c2abe13dc98c2
Michael LeCompte <michaelslec98@gmail.com>
2021-02-13 07:42:42 -0800

    feat: Adding persistent user login

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,36 @@ import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/posts";
 import { UserResolver } from "./resolvers/user";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
 
 async function main() {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
   const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax",
+        secure: __prod__, // cookie only works in https
+      },
+      secret: process.env.SESH_SECRET as string,
+      resave: false,
+    })
+  );
 
   const server = new ApolloServer({
     schema: await buildSchema({
