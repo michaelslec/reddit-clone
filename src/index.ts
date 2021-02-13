f2dfec384b275003340990c50575b2fdcc8a9363
Michael LeCompte <michaelslec98@gmail.com>
2021-02-13 02:57:50 -0800

    feat: update Post model

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import microConfig from "./mikro-orm.config";
 
 async function main() {
   const orm = await MikroORM.init(microConfig);
-
+  await orm.getMigrator().up();
   const post = orm.em.create(Post, { title: "my first post" });
   await orm.em.persistAndFlush(post);
 }
