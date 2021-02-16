0434285e150692e007e6414fb64c943f7779f518
Michael LeCompte <michaelslec98@gmail.com>
2021-02-16 15:27:22 -0800

    feat: add register page

---
+++ reddit-clone-web/src/pages/register.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface registerProps {}
+
+export const Register: React.FC<registerProps> = ({}) => {
+  return <div>register page</div>;
+};
+
+export default Register;
