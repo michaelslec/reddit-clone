5cb1a716e2ded8d475b12a1f48562042dc8ad759
Michael LeCompte <michaelslec98@gmail.com>
2021-02-17 01:18:06 -0800

    fix: needed refresh to see username

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,13 +1,72 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
-import { createClient, Provider } from "urql";
+import { Cache, cacheExchange, QueryInput } from "@urql/exchange-graphcache";
+import { createClient, dedupExchange, fetchExchange, Provider } from "urql";
+import {
+  LoginMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from "../generated/graphql";
 
 import theme from "../theme";
+
+// TODO: study urql cache
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  input: QueryInput,
+  result: any,
+  update: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(input, (data) => update(result, data as any) as any);
+}
 
 const client = createClient({
   url: "http://localhost:3001/graphql",
   fetchOptions: {
     credentials: "include",
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, _args, cache) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.__typename === "FieldError") {
+                  return query;
+                } else {
+                  return {
+                    me: result.login,
+                  };
+                }
+              }
+            );
+          },
+          register: (_result, _args, cache) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.__typename === "FieldError") {
+                  return query;
+                } else {
+                  return {
+                    me: result.register,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
 function MyApp({ Component, pageProps }: any) {
