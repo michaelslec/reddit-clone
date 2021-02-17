1b99e414c2cb8a9b9b34f28a8af4dc6ecafa46ba
Michael LeCompte <michaelslec98@gmail.com>
2021-02-16 17:01:41 -0800

    feat: Add register button

--- reddit-clone-web/src/pages/register.tsx
+++ reddit-clone-web/src/pages/register.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Form, Formik } from "formik";
 import { Wrapper } from "../components/Wrapper";
 import InputField from "../components/InputField";
+import { Box, Button } from "@chakra-ui/react";
 
 interface registerProps {}
 
@@ -12,10 +13,20 @@ const Register: React.FC<registerProps> = ({}) => {
         initialValues={{ username: "", password: "" }}
         onSubmit={(values) => console.log(values)}
       >
-        {({ values, handleChange }) => (
+        {({ isSubmitting }) => (
           <Form>
             <InputField name="username" />
-            <InputField name="password" type="password" />
+            <Box mt={4}>
+              <InputField name="password" type="password" />
+            </Box>
+            <Button
+              mt={4}
+              isLoading={isSubmitting}
+              type="submit"
+              colorScheme="teal"
+            >
+              register
+            </Button>
           </Form>
         )}
       </Formik>
