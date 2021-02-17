4ee2c1fcd80e3b069ad03fa6fcdb9436108e38d9
Michael LeCompte <michaelslec98@gmail.com>
2021-02-16 23:25:49 -0800

    feat: add login page
    Currently, just the register page

---
+++ web/src/pages/login.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Form, Formik } from "formik";
+import { Wrapper } from "../components/Wrapper";
+import InputField from "../components/InputField";
+import { Box, Button } from "@chakra-ui/react";
+import { useRegisterMutation } from "../generated/graphql";
+import { useRouter } from "next/router";
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+  const router = useRouter();
+  const [, register] = useRegisterMutation();
+
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values);
+          const data = response.data?.register;
+          if (data?.__typename === "FieldError")
+            setErrors({
+              username: data.field === "username" ? data.message : "",
+              password: data.field === "password" ? data.message : "",
+            });
+          else router.push("/");
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name="username" />
+            <Box mt={4}>
+              <InputField name="password" type="password" />
+            </Box>
+            <Button
+              mt={4}
+              isLoading={isSubmitting}
+              type="submit"
+              colorScheme="teal"
+            >
+              register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
