65d81b799a8d6fa575277f48fc70436da562ad23
Michael LeCompte <michaelslec98@gmail.com>
2021-02-17 00:33:35 -0800

    feat: some styling to home page

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,8 +1,20 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Link } from "@chakra-ui/react";
 import React, { ReactElement } from "react";
+import NextLink from "next/link";
 
 export interface NavBarProps {}
 
 export default function NavBar(props: NavBarProps): ReactElement | null {
-  return <Box bg="tomato">pretty good</Box>;
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml={"auto"}>
+        <NextLink href="/login">
+          <Link mr={2}>login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>register</Link>
+        </NextLink>
+      </Box>
+    </Flex>
+  );
 }
