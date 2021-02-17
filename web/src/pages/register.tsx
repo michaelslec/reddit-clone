8a7f9cdc44ddfa9bd93d1b4b9b02e4ca1f3a4534
Michael LeCompte <michaelslec98@gmail.com>
2021-02-16 17:27:25 -0800

    git: removed dist

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -3,15 +3,36 @@ import { Form, Formik } from "formik";
 import { Wrapper } from "../components/Wrapper";
 import InputField from "../components/InputField";
 import { Box, Button } from "@chakra-ui/react";
+import { useMutation } from "urql";
 
 interface registerProps {}
 
+const kREGISTER_MUTATION = `mutation Register($username: String!, $password: String!) {
+    register(options: {username: $username, password: $password}) {
+      ... on User {
+        id
+        username
+        createdAt
+      }
+
+      ... on FieldError {
+        field
+        message
+      }
+    }
+  }`;
+
 const Register: React.FC<registerProps> = ({}) => {
+  const [, register] = useMutation(kREGISTER_MUTATION);
+
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={(values) => {
+          console.log(values);
+          register(values);
+        }}
       >
         {({ isSubmitting }) => (
           <Form>
