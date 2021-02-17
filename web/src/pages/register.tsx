02dd83f9360e99d229305aa255215bc65a67a8a8
Michael LeCompte <michaelslec98@gmail.com>
2021-02-16 18:19:19 -0800

    feat: Now using generated urql types for graphql

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -4,33 +4,19 @@ import { Wrapper } from "../components/Wrapper";
 import InputField from "../components/InputField";
 import { Box, Button } from "@chakra-ui/react";
 import { useMutation } from "urql";
+import { useRegisterMutation } from "../generated/graphql";
 
 interface registerProps {}
 
-const kREGISTER_MUTATION = `mutation Register($username: String!, $password: String!) {
-    register(options: {username: $username, password: $password}) {
-      ... on User {
-        id
-        username
-        createdAt
-      }
-
-      ... on FieldError {
-        field
-        message
-      }
-    }
-  }`;
-
 const Register: React.FC<registerProps> = ({}) => {
-  const [, register] = useMutation(kREGISTER_MUTATION);
+  const [, register] = useRegisterMutation();
 
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          return register(values);
+        onSubmit={async (values) => {
+          const response = await register(values);
         }}
       >
         {({ isSubmitting }) => (
