e6e6b5473f8234666471433bc1c8f52fab7a7864
Michael LeCompte <michaelslec98@gmail.com>
2021-02-17 01:19:58 -0800

    feat: change NavBar color

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -31,7 +31,7 @@ export default function NavBar(): ReactElement | null {
     );
 
   return (
-    <Flex bg="tomato" p={4}>
+    <Flex bg="tan" p={4}>
       <Box ml={"auto"}>{body}</Box>
     </Flex>
   );
