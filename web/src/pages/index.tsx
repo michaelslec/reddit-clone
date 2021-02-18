8f1260f47fdc306199fd04b653364d2e258c7352
Michael LeCompte <michaelslec98@gmail.com>
2021-02-18 14:03:59 -0800

    feat: show blog posts on home page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,10 +1,19 @@
 import NavBar from "../components/NavBar";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClients";
+import { usePostsQuery } from "../generated/graphql";
 
-export default function Index() {
+function Index() {
+  const [{ data }] = usePostsQuery();
+
   return (
     <>
       <NavBar />
       <div>Hello world!</div>
+      <br />
+      {!data ? null : data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
     </>
   );
 }
+
+export default withUrqlClient(createUrqlClient)(Index);
