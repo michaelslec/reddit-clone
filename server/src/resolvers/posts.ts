e74d728f1f4751c5e92fc0fd8b100588e61c60bb
Michael LeCompte <michaelslec98@gmail.com>
2021-02-21 11:18:59 -0800

    feat: convert post to typeorm

--- server/src/resolvers/posts.ts
+++ server/src/resolvers/posts.ts
@@ -5,58 +5,36 @@ import { MyCtx } from "src/types";
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  posts(@Ctx() { em }: MyCtx): Promise<Post[]> {
-    return em.find(Post, {});
+  posts(): Promise<Post[]> {
+    return Post.find();
   }
 
   @Query(() => Post, { nullable: true })
-  post(@Arg("id") id: number, @Ctx() { em }: MyCtx): Promise<Post | null> {
-    return em.findOne(Post, { id });
+  post(@Arg("id") id: number): Promise<Post | undefined> {
+    return Post.findOne(id);
   }
 
   @Mutation(() => Post)
-  async createPost(
-    @Arg("title") title: string,
-    @Ctx() { em }: MyCtx
-  ): Promise<Post> {
-    const post = em.create(Post, { title });
-    await em.persistAndFlush(post);
-    return post;
+  async createPost(@Arg("title") title: string): Promise<Post> {
+    return Post.create({ title }).save();
   }
 
   @Mutation(() => Post, { nullable: true })
   async updatePost(
     @Arg("id") id: number,
-    @Arg("title", () => String, { nullable: true }) title: string,
-    @Ctx() { em }: MyCtx
+    @Arg("title", () => String, { nullable: true }) title: string
   ): Promise<Post | null> {
-    const post = await em.findOne(Post, { id });
-    if (!post) {
-      return null;
-    }
-    if (typeof title !== "undefined") {
-      post.title = title;
-      await em.persistAndFlush(post);
-    }
+    const post = await Post.findOne({ id });
+
+    if (!post) return null;
+    if (typeof title !== "undefined") await Post.update({ id }, { title });
 
     return post;
   }
 
   @Mutation(() => Boolean)
-  async deletePost(
-    @Arg("id") id: number,
-    @Ctx() { em }: MyCtx
-  ): Promise<boolean> {
-    let post: Post;
-    try {
-      post = await em.findOneOrFail(Post, { id });
-      console.log(typeof post);
-    } catch (error) {
-      return false;
-    }
-
-    await em.removeAndFlush(post);
-
+  async deletePost(@Arg("id") id: number): Promise<boolean> {
+    await Post.delete(id);
     return true;
   }
 }
