5fab48f4e11688b80bb0d7e0bf406265bf702b7d
Michael LeCompte <michaelslec98@gmail.com>
2021-02-21 14:31:09 -0800

    feat: add create post to home screen

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,22 +1,24 @@
-import NavBar from "../components/NavBar";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClients";
 import { usePostsQuery } from "../generated/graphql";
+import Layout from "../components/Layout";
+import NextLink from "next/link";
+import { Link } from "@chakra-ui/react";
 
 function Index() {
   const [{ data }] = usePostsQuery();
 
   return (
-    <>
-      <NavBar />
-      <div>Hello world!</div>
-      <br />
+    <Layout>
+      <NextLink href="/create-post">
+        <Link>create post</Link>
+      </NextLink>
       {!data ? (
         <div>Loading ...</div>
       ) : (
         data.posts.map((p) => <div key={p.id}>{p.title}</div>)
       )}
-    </>
+    </Layout>
   );
 }
 
