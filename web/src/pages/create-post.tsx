ce8ba3099e74ba3ac39de97e0d2564231a1a6e65
Michael LeCompte <michaelslec98@gmail.com>
2021-02-21 13:24:50 -0800

    feat: auto redirect to login when not logged in

--- web/src/pages/create-post.tsx
+++ web/src/pages/create-post.tsx
@@ -2,17 +2,22 @@ import { Box, Button } from "@chakra-ui/react";
 import { Formik, Form } from "formik";
 import { withUrqlClient } from "next-urql";
 import { useRouter } from "next/router";
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect } from "react";
 import InputField from "../components/InputField";
 import Layout from "../components/Layout";
-import { useCreatePostMutation } from "../generated/graphql";
+import { useCreatePostMutation, useMeQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClients";
 
 export interface CreatePostProps {}
 
 function CreatePost(): ReactElement | null {
+  const [{ data, fetching }] = useMeQuery();
   const [, createPost] = useCreatePostMutation();
   const router = useRouter();
+
+  useEffect(() => {
+    if (!fetching && !data?.me) router.replace("/login");
+  }, [fetching, data, router]);
 
   return (
     <Layout variant="small">
