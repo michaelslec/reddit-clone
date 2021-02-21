7975f97025e0c8b7da47bb94dc777e4dba7e036b
Michael LeCompte <michaelslec98@gmail.com>
2021-02-21 10:19:04 -0800

    feat: basic form

--- web/src/pages/forgot-password.tsx
+++ web/src/pages/forgot-password.tsx
@@ -1,7 +1,38 @@
+import { Button } from "@chakra-ui/react";
+import { Formik, Form } from "formik";
+import { withUrqlClient } from "next-urql";
 import React, { ReactElement } from "react";
+import InputField from "../components/InputField";
+import { Wrapper } from "../components/Wrapper";
+import { createUrqlClient } from "../utils/createUrqlClients";
 
-export interface forgotPasswordProps {}
-
-export default function forgotPassword(): ReactElement | null {
-  return <div>Hello</div>;
+function ForgotPassword(): ReactElement | null {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ usernameOrEmail: "", password: "" }}
+        // onSubmit={async (values, { setErrors }) => {}}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="usernameOrEmail"
+              label="Username or Email"
+              placeholder="username"
+            />
+            <Button
+              mt={4}
+              isLoading={isSubmitting}
+              type="submit"
+              colorScheme="teal"
+            >
+              register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
 }
+
+export default withUrqlClient(createUrqlClient)(ForgotPassword);
