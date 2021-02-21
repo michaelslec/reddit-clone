73067fbc9f235d4f20574a3ac673892f8f833f8e
Michael LeCompte <michaelslec98@gmail.com>
2021-02-21 07:47:10 -0800

    feat: display token on page

---
+++ web/src/pages/change-password/[token].tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
+  return <div>{token}</div>;
+};
+
+ChangePassword.getInitialProps = ({ query }) => {
+  return {
+    token: query.token as string,
+  };
+};
+
+export default ChangePassword;
