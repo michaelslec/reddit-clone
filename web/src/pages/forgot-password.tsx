0a10e45189cd891c34bcbfe0eb9eae2e267f836a
Michael LeCompte <michaelslec98@gmail.com>
2021-02-21 10:04:05 -0800

    feat: adding forgot-password page

---
+++ web/src/pages/forgot-password.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from "react";
+
+export interface forgotPasswordProps {}
+
+export default function forgotPassword(): ReactElement | null {
+  return <div>Hello</div>;
+}
