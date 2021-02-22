d40efb55cc82f7c8754a527991be85a3a570c302
Michael LeCompte <michaelslec98@gmail.com>
2021-02-21 16:24:36 -0800

    feat: simple post styling

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,7 +3,8 @@ import { createUrqlClient } from "../utils/createUrqlClients";
 import { usePostsQuery } from "../generated/graphql";
 import Layout from "../components/Layout";
 import NextLink from "next/link";
-import { Link } from "@chakra-ui/react";
+import { Box, Heading, Link, Stack, Text } from "@chakra-ui/react";
+import React from "react";
 
 function Index() {
   const [{ data }] = usePostsQuery({
@@ -20,7 +21,24 @@ function Index() {
       {!data ? (
         <div>Loading ...</div>
       ) : (
-        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+        <Stack spacing={6}>
+          {data.posts.map((post) => (
+            <Box
+              key={post.id}
+              p={5}
+              shadow="md"
+              borderWidth="1px"
+              flex="1"
+              borderRadius="md"
+            >
+              <Heading fontSize="4xl">{post.title}</Heading>
+              <hr />
+              <Text mt={6} noOfLines={6}>
+                {post.text}
+              </Text>
+            </Box>
+          ))}
+        </Stack>
       )}
     </Layout>
   );
