7fbdbe02bbf5081f6712a2b8c70e6902628b432b
Michael LeCompte <michaelslec98@gmail.com>
2021-02-24 01:18:15 -0800

    feat: NavBar is server side

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -2,13 +2,12 @@ import { Box, Button, Flex, HStack, Link } from "@chakra-ui/react";
 import React, { ReactElement } from "react";
 import NextLink from "next/link";
 import { useMeQuery, useLogoutMutation } from "../generated/graphql";
-import isServer from "../utils/isServer";
 
 export interface NavBarProps {}
 
 export default function NavBar(): ReactElement | null {
   const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
-  const [{ data, fetching }] = useMeQuery({ pause: isServer() });
+  const [{ data, fetching }] = useMeQuery();
 
   let body = null;
 
