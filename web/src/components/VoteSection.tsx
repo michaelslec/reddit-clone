817883e50314a3913b7551802add9baf87dbc65d
Michael LeCompte <michaelslec98@gmail.com>
2021-02-23 17:09:44 -0800

    feat: added actual voting functionality

--- web/src/components/VoteSection.tsx
+++ web/src/components/VoteSection.tsx
@@ -1,24 +1,40 @@
 import { ArrowUpIcon, ArrowDownIcon } from "@chakra-ui/icons";
 import { Box, Flex, IconButton } from "@chakra-ui/react";
 import React, { ReactElement } from "react";
+import { useVoteMutation } from "../generated/graphql";
 
 export interface VoteSectionProps {
   points: number;
+  id: number;
 }
 
 export default function VoteSection({
   points,
+  id,
 }: VoteSectionProps): ReactElement | null {
+  const [, voteFn] = useVoteMutation();
   return (
     <Box pr={3}>
       <Flex direction="column" align="center">
         <IconButton
+          onClick={() =>
+            voteFn({
+              postId: id,
+              value: 1,
+            })
+          }
           aria-label="Up vote"
           icon={<ArrowUpIcon />}
           variant="unstyled"
         />
         {points}
         <IconButton
+          onClick={() =>
+            voteFn({
+              postId: id,
+              value: -1,
+            })
+          }
           aria-label="Down vote"
           icon={<ArrowDownIcon />}
           variant="unstyled"
