843f1d8f9d60f55e1556aa77a57983102d54b79a
Michael LeCompte <michaelslec98@gmail.com>
2021-02-25 20:10:54 -0800

    refactor: added another custom hook for id

--- web/src/utils/useGetPostFromUrl.ts
+++ web/src/utils/useGetPostFromUrl.ts
@@ -1,17 +1,12 @@
-import { useRouter } from "next/router";
 import { usePostQuery } from "../generated/graphql";
-
-const router = useRouter();
+import useGetIntId from "./useGetIntId";
 
 export default function useGetPostFromUrl() {
-  const intId =
-    typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
-  const query = usePostQuery({
+  const intId = useGetIntId();
+  return usePostQuery({
     pause: intId === -1,
     variables: {
       id: intId,
     },
   });
-
-  return { id: intId, ...query };
 }
