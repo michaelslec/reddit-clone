5833581f2b7fef583c4f83e247568e6a8ea382ed
Michael LeCompte <michaelslec98@gmail.com>
2021-02-26 06:44:32 -0800

    fix: cookies, hopefully

--- server/src/index.ts
+++ server/src/index.ts
@@ -36,7 +36,7 @@ async function main() {
   const RedisStore = connectRedis(session);
 
   const redis = new Redis(process.env.REDIS_URL);
-  app.set("proxy", 1);
+  app.set("trust proxy", 1);
   app.use(
     cors({
       origin: process.env.CORS_ORIGIN,
