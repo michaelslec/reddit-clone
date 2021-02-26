73e5968cccd0969f5fc32c46460c400c557ec1d0
Michael LeCompte <michaelslec98@gmail.com>
2021-02-25 20:15:01 -0800

    feat: basic update functionality

--- web/src/pages/post/edit/[id].tsx
+++ web/src/pages/post/edit/[id].tsx
@@ -1,9 +1,72 @@
+import { Box, Button } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
 import { withUrqlClient } from "next-urql";
-import { ReactElement } from "react";
+import React, { ReactElement } from "react";
+import InputField from "../../../components/InputField";
+import Layout from "../../../components/Layout";
+import {
+  usePostQuery,
+  useUpdatePostMutation,
+} from "../../../generated/graphql";
 import { createUrqlClient } from "../../../utils/createUrqlClient";
+import useGetIntId from "../../../utils/useGetIntId";
 
 function EditPost(): ReactElement | null {
-  return <div>hello</div>;
+  const intId = useGetIntId();
+  const [{ data, fetching }] = usePostQuery({
+    variables: {
+      id: intId,
+    },
+  });
+  const [, updatePost] = useUpdatePostMutation();
+
+  if (fetching)
+    return (
+      <Layout>
+        <div>Loading...</div>
+      </Layout>
+    );
+
+  if (!data?.post) {
+    return (
+      <Layout>
+        <Box>Could not find post</Box>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout variant="small">
+      <Formik
+        initialValues={{ title: data?.post?.title, text: data?.post?.text }}
+        onSubmit={async (values) => {
+          updatePost({ id: intId, ...values });
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name="title" />
+            <Box mt={4}>
+              <InputField
+                textArea
+                name="text"
+                label="Body"
+                placeholder="text..."
+              />
+            </Box>
+            <Button
+              mt={4}
+              isLoading={isSubmitting}
+              type="submit"
+              colorScheme="teal"
+            >
+              Update Post
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Layout>
+  );
 }
 
 export default withUrqlClient(createUrqlClient)(EditPost);
