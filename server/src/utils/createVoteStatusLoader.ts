b1fe72163b1ed9932d309b162557e0e6c3cd7bf0
Michael LeCompte <michaelslec98@gmail.com>
2021-02-25 21:15:20 -0800

    feat: dataloader for vote status

---
+++ server/src/utils/createVoteStatusLoader.ts
@@ -0,0 +1,16 @@
+import DataLoader from "dataloader";
+import { Upper } from "../entities/Upper";
+
+export default function createVoteStatusLoader() {
+  return new DataLoader<{ postId: number; userId: number }, Upper | null>(
+    async (keys) => {
+      const uppers = await Upper.findByIds(keys as any);
+      const upperIdsToUpper: Record<string, Upper> = {};
+      uppers.forEach((upper) => {
+        upperIdsToUpper[`${upper.userId}|${upper.postId}`] = upper;
+      });
+
+      return keys.map((key) => upperIdsToUpper[`${key.userId}|${key.postId}`]);
+    }
+  );
+}
