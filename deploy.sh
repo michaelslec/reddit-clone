6d8a348c01f50377022e8c0ac17814db3849a3e2
Michael LeCompte <michaelslec98@gmail.com>
2021-02-27 08:04:49 -0800

    feat: deploy offers usage menu and -b flag

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-  echo -n "What version? "
-  read VER
-else
-  VER=$1
-fi
+NO_DEPLOY=0
 
+usage() {
+  cat << EOF
+  Usage: $(basename "$0") [-b] [version]
+  Deploy this project to Digital Ocean
+
+  -h,  Display this message
+  -b,  Just build. Do not send this container to DigitalOcean droplet.
+
+EOF
+  exit $1
+}
+
+build() {
+  docker build -t $NEW_BUILD server
+}
+
+deploy() {
+  docker push $NEW_BUILD
+  ssh reddit-clone "docker pull ${NEW_BUILD} && docker tag ${NEW_BUILD} \
+    dokku/reddit-clone:${VER} && dokku deploy reddit-clone ${VER}"
+}
+
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    -h) usage 0;;
+    -b) NO_DEPLOY=1;;
+     *) VER=$1
+  esac
+  shift
+done
+
+[ -z $VER ] && read -p "What version? " VER
 NEW_BUILD="michaelslec/reddit-clone:${VER}"
 
-docker build -t $NEW_BUILD server
-docker push $NEW_BUILD
-
-ssh reddit-clone "docker pull ${NEW_BUILD} && docker tag ${NEW_BUILD} dokku/reddit-clone:${VER} && dokku deploy reddit-clone ${VER}"
+build
+[ $NO_DEPLOY -eq 0 ] && deploy
